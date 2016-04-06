55c4f382a994d394be416c12287f9bff047566d8
arifin <vynhart@gmail.com>
2016-04-06 23:31:10 +0700

    add styling to proposal form

--- application/views/proposal/_form.php
+++ application/views/proposal/_form.php
@@ -1,17 +1,28 @@
+<?php $general_attr = array('class' => 'form-control'); ?>
+
+<div class="row">
+<div class="col-sm-6">
 <?= form_open('/proposal/create_proposal'); ?>
-  <?= form_label("Tujuan Penggunaan Hibah", "proposal['purpose']"); ?>
-  <?= form_textarea("proposal['purpose']") ?>
+  <div class="form-group">
+    <?= form_label("Tujuan Penggunaan Hibah", "proposal[purpose]"); ?>
+    <?= form_textarea("proposal[purpose]", set_value("proposal[purpose]"), $general_attr) ?>
+  </div>
 
-  <?= form_label("Jumlah yang Dibutuhkan", "proposal['needed_amount']"); ?>
-  <?= form_input("proposal['needed_amount']") ?>
+  <div class="form-group">
+    <?= form_label("Jumlah yang Dibutuhkan", "proposal[needed_amount]"); ?>
+    <?= form_input("proposal[needed_amount]", set_value("proposal[needed_amount]"), $general_attr) ?>
+  </div>
 
-  <?= form_fieldset("List Semua Kebutuhan") ?>
-    <?= form_label("List Semua Anggota", "proposal['membership']"); ?>
-    <?= form_input("proposal['needs']") ?>
-  <?= form_fieldset_close() ?>
+  <div class="form-group">
+    <?= form_fieldset("List Semua Kebutuhan") ?>
+      <?= form_input("proposal[needs][0]", set_value("proposal[needs][0]"), $general_attr) ?>
+    <?= form_fieldset_close() ?>
+  </div>
 
-  <?= form_label("Penanggung Jawab Proposal", "proposal['pic']"); ?>
-  <?= form_input("proposal['pic']") ?>
+  <div class="form-group">
+    <?= form_label("Penanggung Jawab Proposal", "proposal[pic]"); ?>
+    <?= form_input("proposal[pic]", set_value("proposal[pic]"), $general_attr) ?>
+  </div>
 
   <?php
 $agreement = "Dengan ini saya menyatakan bahwa dalam penggunaan dana
@@ -23,8 +34,15 @@ Seandainya saya tidak menyampaikan pertanggung jawaban, saya bertanggung jawab
 untuk mengembalikan dana hibah tersebut ke pemerintahan daerah dan bersedia
 dituntut sesuai dengan hukum yang berlaku.";
   ?>
-  <?= form_label($agreement, "proposal['agreement']"); ?>
-  <?= form_checkbox("proposal['agreement']", '1') ?>
 
-  <?= form_submit("submit", "Submit") ?>
+  <div class="form-group">
+    <?= form_checkbox("proposal[agreement]", '1', $general_attr) ?><?= $agreement ?>
+  </div>
+
+   <div class="form-group">
+    <?= form_submit("submit", "Submit", array('class' => 'btn btn-primary')) ?>
+    <?= anchor('', 'Kembali', array('class' => 'btn btn-default')); ?>
+  </div>
 <?= form_close(); ?>
+</div>
+</div>
