7b0c483183e1b0841dbb11afc9911393c1ca25a1
arifin <vynhart@gmail.com>
2016-04-17 16:48:49 +0700

    fix migrate Applicant

--- application/migrations/20160403162800_add_applicant.php
+++ application/migrations/20160403162800_add_applicant.php
@@ -36,20 +36,19 @@ class Migration_Add_applicant extends CI_Migration{
         'type' => 'VARCHAR',
         'constraint' => '255'
       ),
-
       'group_id' => array(
         'type' => 'INT',
-        'null' => 'TRUE'
+        'constraint' => '255',
+        'null' => TRUE
       ),
-
       'username' => array(
         'type' => 'VARCHAR',
         'constraint' => '255',
-        'null' => 'TRUE'
+        'null' => TRUE
       ),
       'password' => array(
         'type' => 'TEXT',
-        'null' => 'TRUE'
+        'null' => TRUE
       ),
     ));
     $this->dbforge->add_key('id', TRUE);
